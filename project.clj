65aa3c16a7f920009e2c9997ef21d4684e22d155
Coda Hale <coda.hale@gmail.com>
2016-12-06 16:03:15 -0800

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codahale/u2f-clj "0.1.0"
+(defproject com.codahale/u2f-clj "0.1.1-SNAPSHOT"
   :description "A library for implementing FIDO U2F."
   :url "https://github.com/codahale/u2f-clj"
   :license {:name "Eclipse Public License"
